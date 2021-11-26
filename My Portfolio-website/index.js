71dd402c7d2ae5bb27ffac3746df8a3b8a4bd9b5
Shivansh Agrawal <95066312+Shivansh46@users.noreply.github.com>
2021-11-26 20:49:55 +0530

    Add files via upload

---
+++ My Portfolio-website/index.js
@@ -0,0 +1,28 @@
+function open_instagram(){
+    window.open( "https://www.instagram.com/__rahat_046_/?hl=en", 'self');
+}
+function open_facebook(){
+    window.open( "https://www.facebook.com/profile.php?id=100005562076813", 'self');
+}
+function open_github(){
+    window.open( "https://github.com/Shivansh46", 'self');
+}
+function open_codeforces(){
+    window.open( "website", 'self');
+}
+function open_linkedin(){
+    window.open( "https://www.linkedin.com/in/shivansh-agrawal-75171221a/", 'self');
+    
+}
+function homepage(){
+    window.open( "index.html", 'self');
+}
+
+
+
+function scroll_left(){
+    document.getElementById('interest_223').scrollLeft -= 100;
+}
+function scroll_right(){
+    document.getElementById('interest_223').scrollLeft += 100;
+}
